f49da9480972ea150d391f6e6acceb25e7b3c1fe
Asemco <crasykid2@hotmail.com>
2019-07-10 02:34:10 -0400

    AutoUpdater started.  This was before the current one was put into place.

---
+++ AutoUpdater.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IronWebScraper;
+
+namespace TFT_Overlay
+{
+    class AutoUpdater : WebScraper
+    {
+        const string OVERLAY_PULSE_URL = "https://github.com/Just2good/TFT-Overlay/pulse";
+        private string version = MainWindow.APP_VERSION;
+
+        public async void checkVersionAsync()
+        {
+            this.ObeyRobotsDotTxt = false;
+            await this.StartAsync();
+        }
+
+        public void compareVersion(string newVersion)
+        {
+            bool currentVersionIsNewestVersion = version == newVersion;
+            if (!currentVersionIsNewestVersion)
+            {
+                System.Windows.MessageBox.Show("The newest version is " + newVersion + ". Your version is " + version + ": " + currentVersionIsNewestVersion);
+            }
+        }
+
+        public override void Init()
+        {
+            this.LoggingLevel = WebScraper.LogLevel.All;
+            this.Request(OVERLAY_PULSE_URL, Parse);
+        }
+
+        public override void Parse(Response response)
+        {
+            // All EVEN indexes are \n.  All ODD indexes contain actual content.
+            // This is factual as of 2019-07-04, and should be until Github changes their page.
+            // There is no reliable css to attach to, so we grab the highest element with an ID
+            // and head down until we can isolate the version number in the format: V{Number}.
+            try
+            {
+                HtmlNode releasesSection = response.GetElementById("releases");
+                HtmlNode releases = releasesSection.ChildNodes[3];
+                HtmlNode latestRelease = releases.ChildNodes[releases.ChildNodes.Length - 2];
+                string latestVersion = latestRelease.ChildNodes[3].TextContentClean;
+                Scrape(new ScrapedData() { { "Version", latestVersion } });
+                compareVersion(latestVersion);
+            }
+            catch (Exception excep)
+            {
+                // If it failed for any reason, inform the user to check the github and download it from there.
+                // If the Github's no longer working, yikes.
+                //System.Windows.MessageBox.Show("Please check the github directly for a new update!  The URL is: https://github.com/Just2good/TFT-Overlay");
+            }
+        }
+    }
+}
+        
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -21,18 +21,24 @@ namespace TFT_Overlay
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
-
     {
+        public const string APP_VERSION = "V1.7.2";
+        public bool githubChecked = false;
         bool canDrag = true;
         bool onTop = true;
-        
+        AutoUpdater scraper = new AutoUpdater();
+
         public MainWindow()
         {
+            scraper.checkVersionAsync();
             InitializeComponent();
             MouseLeftButtonDown += new MouseButtonEventHandler(MainWindow_MouseLeftButtonDown);
-
         }
 
+        public void editGithubChecked(bool isChecked)
+        {
+            githubChecked = isChecked;
+        }
 
         private void MenuItem_Click(object sender, RoutedEventArgs e)
         {
@@ -41,7 +47,7 @@ namespace TFT_Overlay
 
         private void MenuItem_Click_About(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("TFT Information Overlay V1.7.2 by J2GKaze/Jinsoku#4019.\n\nDM me on Discord if you have any questions.\n\nBig thanks to Chaoticoz for Lock Window, Always on Top, and Mouseover.\n\nAlso thanks to, Asemco/Asemco#7390 for adding Origins and Classes!\n\nLast Updated: July 2nd, 2019 @ 8:22PM PST");
+            MessageBox.Show("TFT Information Overlay " + APP_VERSION + " by J2GKaze/Jinsoku#4019.\n\nDM me on Discord if you have any questions.\n\nBig thanks to Chaoticoz for Lock Window, Always on Top, and Mouseover.\n\nAlso thanks to, Asemco/Asemco#7390 for adding Origins and Classes!\n\nLast Updated: July 2nd, 2019 @ 8:22PM PST");
         }
 
         private void MenuItem_Click_Lock(object sender, RoutedEventArgs e)
